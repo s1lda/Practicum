9cea4a7c2539a26806c019368d649cb2fd577cf8
Damir Sagalbaev <105151275+s1lda@users.noreply.github.com>
2024-05-03 00:25:07 +0600

    Add files via upload

---
+++ practicum.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import pickle
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score
+from sklearn.metrics import log_loss, roc_auc_score, confusion_matrix, classification_report
+import numpy as np;
+from sklearn.preprocessing import StandardScaler
+
+model_save_path = 'C:/Users/ZALMAN/Downloads/'
+
+data_regression = pd.read_csv('C:/Users/ZALMAN/Downloads/housing23.csv')
+
+data_classification = pd.read_csv("C:/Users/ZALMAN/Downloads/smoke2.csv")
+
+
+
+
+
+def load_models():
+    model_classification = pickle.load(open(model_save_path + 'Ridge.pkl', 'rb'))
+    model_regression = pickle.load(open(model_save_path + 'TweedieRegressor.pkl', 'rb'))
+    return model_classification, model_regression
+
+def machine_learning():
+    st.title("Datasets")
+    uploaded_file = st.file_uploader("Загрузите ваш CSV файл", type="csv")
+    if uploaded_file is not None:
+        if uploaded_file.name == "smoke2.csv":
+            st.write("Файл smoke2.csv был загружен")
+            y = data_classification["Fire Alarm_Yes"]
+            X = data_classification.drop(["Fire Alarm_Yes","UTC"], axis=1)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
+            model_classification = pickle.load(open(model_save_path + 'Ridge.pkl', 'rb'))
+            predictions_classification = model_classification.predict(X_test)
+            accuracy_classification = accuracy_score(y_test, predictions_classification)
+            st.success(f"Точность: {accuracy_classification}")
+
+        elif uploaded_file.name == "housing23.csv":
+            st.write("Файл housing23.csv был загружен")
+            model_regression = pickle.load(open(model_save_path + 'TweedieRegressor.pkl', 'rb'))
+            data_regression = pd.read_csv('C:/Users/ZALMAN/Downloads/housing23.csv')
+            y = data_regression["price"]
+            X = data_regression.drop(["price"], axis=1)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=0)
+            predictions_regression = model_regression.predict(X_test)
+            r2_score_regression = r2_score(y_test, predictions_regression)
+            st.success(f"Коэффициент детерминации (R²): {r2_score_regression}")
+
+
+        else:
+            st.write("Загружен файл неизвестного формата")
+
+if __name__ == "__main__":
+    machine_learning()
